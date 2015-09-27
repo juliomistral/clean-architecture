ba73ee31e2287e486a1998e184f2bf7390dfed42
Julio A. Mistral <julio.mistral@gmail.com>
2015-09-27 09:33:14 -0700

    Remove common code and create a dependency on the commons project

--- src/main/java/org/cleanarchitecture/common/domain/Validatable.java
+++
@@ -1,8 +0,0 @@
-package org.cleanarchitecture.common.domain;
-
-import javax.validation.ConstraintViolationException;
-
-public interface Validatable {
-
-    void validate() throws ConstraintViolationException;
-}
